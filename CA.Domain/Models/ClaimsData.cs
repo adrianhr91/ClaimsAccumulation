5567def214fe07cfa20b222ad079cde18073b32d
Adrian Hristov <adrianhr91@gmail.com>
2016-02-12 22:29:53 +0000

    Added solution and Domain project

---
+++ CA.Domain/Models/ClaimsData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CA.Domain.Models
+{
+    public class ClaimsData
+    {
+        public List<Product> Products { get; private set; }
+
+        public ClaimsData()
+        {
+            Products = new List<Product>();
+        }
+
+        public void AddDevelopment(string productName, Product.Claim claim)
+        {
+
+            if (Products.Any(prod => prod.ProductName == productName))
+            {
+                UpdateProduct(productName, claim);
+            }
+            else
+            {
+                AddProduct(productName, claim);
+            }
+        }
+
+        private void UpdateProduct(string productName, Product.Claim claim)
+        {
+            var product = Products.Single(prod => prod.ProductName == productName);
+            product.Claims.Add(claim);
+        }
+
+        private void AddProduct(string productName, Product.Claim claim)
+        {
+            var product = new Product(productName);
+            product.Claims.Add(claim);
+
+            Products.Add(product);
+        }
+    }
+}
---
+++ CA.Domain/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CA.Domain.Models
+{
+    public class Product
+    {
+        public string ProductName { get; private set; }
+        public List<Claim> Claims { get; private set; }
+
+        public Product(string productName)
+        {
+            ProductName = productName;
+            Claims = new List<Claim>();
+        }
+
+        public class Claim
+        {
+            public int OriginYear { get; private set; }
+            public int DevelopmentYear { get; private set; }
+            public decimal IncrementalValue { get; private set; }
+
+            public Claim(int originYear, int developmentYear, decimal incrementalValue)
+            {
+                OriginYear = originYear;
+                DevelopmentYear = developmentYear;
+                IncrementalValue = incrementalValue;
+            }
+        }
+    }
+}
